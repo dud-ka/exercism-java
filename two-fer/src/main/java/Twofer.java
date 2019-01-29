a730aaa139d7e1ced28ebdec17882751f99dd699
Karolina Dudzik <dudzik.karolina@outlook.com>
2019-01-29 10:16:18 +0100

    two-fer fix

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,12 +1,10 @@
 class Twofer {
-
-
-
     String twofer(String name) {
+        String sentencePart2 = "one for me.";
         if (name == null) {
             return "One for you, one for me.";
         } else {
-            return "One for " +  name +", one for me.";
+            return String.format("One for %1$s, %2$s", name, sentencePart2);
         }
     }
 }
